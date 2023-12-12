96a8f770be803886db766cb762a941f3c33dd4ba
Dheerendranath <67054266+Dheerendranath@users.noreply.github.com>
2023-12-12 14:17:46 -0600

    Add files via upload

---
+++ general_tumor_intensity.m
@@ -0,0 +1,105 @@
+clear all
+close all
+% Set up the main directory and target subdirectories
+main_directory = '/Users/dheerendranathbattalapalli/Desktop/BRATS_2018_dataset/MICCAI_BraTS_2018_Data_Training/HGG/';
+subdirs = 1:150;
+
+% Loop over the subdirectories
+for idx = 1:length(subdirs)
+    subdir = subdirs(idx);
+    folder_path = fullfile(main_directory, num2str(subdir));
+    
+    % Find the relevant NIfTI files in the current folder
+    t1ce_file = dir(fullfile(folder_path, '*t1ce.nii.gz'));
+    label_file = dir(fullfile(folder_path, '*seg.nii.gz'));
+    
+    % If the NIfTI files are missing, continue to the next folder
+    if isempty(t1ce_file) || isempty(label_file)
+        fprintf('Missing t1ce.nii.gz or seg.nii.gz in folder: %s\n', folder_path);
+        continue;
+    end
+    
+    t1ce_path = fullfile(folder_path, t1ce_file(1).name);
+    label_path = fullfile(folder_path, label_file(1).name);
+    
+% Load the NIfTI files
+    t1ce_data = niftiread(t1ce_path);
+    label_data = niftiread(label_path);
+
+% Initialize the 3D skeleton
+    skeleton = zeros(size(label_data));
+
+% Perform morphological operations for each slice to extract the skeletal structure
+    for slice = 1:size(label_data, 3)
+        binary_slice = label_data(:,:,slice) > 0;
+        skeleton(:,:,slice) = bwmorph(binary_slice, 'skel', Inf);
+    end
+
+% Overlay the skeletal structure on the T1ce image
+    overlay_image = double(t1ce_data) .* double(skeleton);
+    
+    % Overlay the label data on the T1ce image
+    if isa(t1ce_data, 'double')
+        overlay_image = t1ce_data .* double(label_data);
+    else
+        overlay_image = int16(t1ce_data) .* int16(label_data);
+    end
+
+    
+    % Extract the first-order statistical values from the ROI
+    roi_pixels = overlay_image(label_data > 0);
+    roi_pixels_double = double(roi_pixels);
+    
+    %variance_val = var(roi_pixels_double);
+
+    skewness_val = skewness(double(roi_pixels));
+    kurtosis_val = kurtosis(double(roi_pixels));
+    entropy_val = entropy(double(roi_pixels));
+    min_val = min(roi_pixels);
+    max_val = max(roi_pixels);
+    range_val = range(roi_pixels);
+    mad_val = mad(roi_pixels, 1);
+    rms_val = rms(roi_pixels);
+    total_energy_val = sum(roi_pixels.^2);
+    uniformity_val = sum(histcounts(roi_pixels).^2)/numel(roi_pixels);
+    variance_val = var(double(roi_pixels));
+    std_val = std(double(roi_pixels));
+    mean_val = mean(double(roi_pixels));
+    cv_val = std_val/mean_val; % Coefficient of variation
+    % Extract more first-order statistical values from the ROI
+    mode_val = mode(roi_pixels_double);
+    median_val = median(roi_pixels_double);
+    iqr_val = iqr(roi_pixels_double);
+    perc10_val = prctile(roi_pixels_double, 10);
+    perc90_val = prctile(roi_pixels_double, 90);
+    energy_val = sum(roi_pixels_double.^2);
+    robust_mad_val = mad(roi_pixels_double(roi_pixels_double >= perc10_val & roi_pixels_double <= perc90_val), 1);
+
+    
+    % Save the extracted values in a text file for each folder
+    outfile = fullfile(folder_path, 'first_skeletal_t1ce_order_statistics.txt');
+    fid = fopen(outfile, 'w');
+    fprintf(fid, 'Mean: %f\n', mean_val);
+    fprintf(fid, 'Variance: %f\n', variance_val);
+    fprintf(fid, 'Skewness: %f\n', skewness_val);
+    fprintf(fid, 'Kurtosis: %f\n', kurtosis_val);
+    fprintf(fid, 'Entropy: %f\n', entropy_val);
+    fprintf(fid, 'Minimum: %d\n', min_val);
+    fprintf(fid, 'Maximum: %d\n', max_val);
+    fprintf(fid, 'Range: %d\n', range_val);
+    fprintf(fid, 'Mean Absolute Deviation: %f\n', mad_val);
+    fprintf(fid, 'Root Mean Square: %f\n', rms_val);
+    fprintf(fid, 'Total Energy: %f\n', total_energy_val);
+    fprintf(fid, 'Uniformity: %f\n', uniformity_val);
+    fprintf(fid, 'Coefficient of Variation: %f\n', cv_val);
+    fprintf(fid, 'Mode: %f\n', mode_val);
+    fprintf(fid, 'Median: %f\n', median_val);
+    fprintf(fid, 'Interquartile Range: %f\n', iqr_val);
+    fprintf(fid, '10th Percentile: %f\n', perc10_val);
+    fprintf(fid, '90th Percentile: %f\n', perc90_val);
+    fprintf(fid, 'Energy: %f\n', energy_val);
+    fprintf(fid, 'Robust Mean Absolute Deviation: %f\n', robust_mad_val);
+    fclose(fid);
+end
+
+fprintf('Feature extraction completed!\n');
